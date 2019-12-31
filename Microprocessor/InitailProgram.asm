3ee569aa8a1a592e7fd262420085bf4236f4fe2e
AB-AppDev <59381252+AB-AppDev@users.noreply.github.com>
2019-12-31 08:56:29 +0530

    Assembly Program - Hello Worlds!

---
+++ Microprocessor/InitailProgram.asm
@@ -0,0 +1,29 @@
+;64bit program helloworld!------------------------------------------------------------------------
+
+
+section .data
+	hello db 'Hello World!',10
+	helloLen: equ $-hello
+
+section .text
+global _start
+_start:
+	mov rax,1
+	mov rdi,1
+	mov rsi,hello
+	mov rdx,helloLen
+	syscall
+	mov rax,60
+	xor rdi,rdi
+	syscall
+	
+  
+;-------------------------------------------------------------------------------------------------  
+;Output-
+;student@IOE-HWLAB:~/Documents/ABHI rno.43$ nasm -f elf32 mprno43_intr1_64b.asm -o hello.o
+;mprno43_pr1_64b.asm:9-15: error: instruction not supported in 32-bit mode
+;student@IOE-HWLAB:~/Documents/ABHI rno.43$ nasm -f elf64 mprno43_intr1_64b.asm -o hello.o
+;student@IOE-HWLAB:~/Documents/ABHI rno.43$ ld -o hello hello.o
+;student@IOE-HWLAB:~/Documents/ABHI rno.43$ ./hello
+;Hello World!
+
